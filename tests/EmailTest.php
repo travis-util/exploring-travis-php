73d17c91264bb94c5b3c2079ad9f29a9bbf8a0f8
Nardberjean <Nardberjean@users.noreply.github.com>
2018-03-24 08:18:34 -0400

    Update EmailTest.php

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -6,7 +6,7 @@ final class EmailTest extends TestCase
     public function testCanBeCreatedFromValidEmailAddress()
     {
         $this->assertInstanceOf(
-            Email::class,
+            'Email', //Email::class,
             Email::fromString('user@example.com')
         );
     }
